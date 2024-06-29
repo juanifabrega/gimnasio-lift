4a739c403dea195129cc48fefac62fcbd52a76cd
juanifabrega <juanifabrega1997@gmail.com>
2024-06-29 16:46:24 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './componentes-comunes/home/home.component';
+import { ContactoComponent } from './componentes-comunes/contacto/contacto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'contacto', component: ContactoComponent},
+    //siempre al final.
+    {path: '', redirectTo: '/home', pathMatch:'full'}
+];
